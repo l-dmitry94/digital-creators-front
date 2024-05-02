3a92149fdd5122e213ef336e97cf9c3c88c6eb11
Liubov <drogaltseva92@gmail.com>
2024-05-03 00:20:04 +0200

    fix lint

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,7 +13,6 @@ const Sidebar = ({ isActiveSidebar, handleClick }) => {
         setActive(isActiveSidebar);
     }, [isActiveSidebar]);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     const handleClickOutside = event => {
         if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
             setActive(false);
