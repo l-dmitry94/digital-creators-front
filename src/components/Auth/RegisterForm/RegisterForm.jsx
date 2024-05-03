9ccd03b4ba571b7e1f3343fd8944bb041a6a57be
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-05-03 23:38:33 +0300

    update

--- src/components/Auth/RegisterForm/RegisterForm.jsx
+++ src/components/Auth/RegisterForm/RegisterForm.jsx
@@ -8,113 +8,225 @@ import { signup } from '../../../redux/auth/auth-operations';
 import Container from '../../Container';
 import CustomInput from '../../CustomInput/CustomInput';
 import css from './RegisterForm.module.scss';
-// import icons from '../../../assets/icons/icons.svg';
+import icons from '../../../assets/icons/icons.svg';
 import SubmitButton from '../../SubmitButton/SubmitButton';
 import { NavLink } from 'react-router-dom';
 
-const RegistrationForm = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [showPassword, SetShowPassword] = useState(false);
+import { useFormik } from 'formik';
 
-    const passwordVisibility = () => {
-        SetShowPassword(!showPassword);
+const RegistrationForm = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const dispatch = useDispatch();
+
+    const initialValues = {
+        username: '',
+        email: '',
+        password: '',
     };
 
-    const schema = yup.object().shape({
-        name: yup.string().required('Name is required'),
+    const validationSchema = yup.object().shape({
+        username: yup.string().required('Name is required'),
         email: yup
             .string()
             .email('Invalid email')
             .required('Email is required'),
+
         password: yup
             .string()
             .min(8, 'Password must be at least 8 characters')
             .required('Password is required'),
     });
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: yupResolver(schema),
+    const onSubmit = async values => {
+        console.log(dispatch(signup(values)));
+    };
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validationSchema,
     });
 
-    const onSubmit = async data => {
-        try {
-            await dispatch(signup(data));
-            navigate('/home');
-        } catch (error) {
-            console.error(error);
-        }
-        console.log(data);
+    const passwordVisibility = () => {
+        setShowPassword(!showPassword);
     };
 
     return (
-        <div className={css.register_page}>
-            <Container>
-                <div className={css.container}>
-                    <div>
-                        <ul className={css.auth_nav}>
-                            <li>
-                                <NavLink to="/auth/register">
-                                    Registration
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/auth/login">Log In</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                    <form
-                        onSubmit={handleSubmit(onSubmit)}
-                        className={css.auth_form}
-                    >
-                        <div className={css.auth_div}>
-                            <CustomInput
-                                placeholder="Enter your name"
-                                {...register('name')}
-                                className={css.input}
-                            />
-                            {errors.name && (
-                                <span className="error-message">
-                                    {errors.name.message}
-                                </span>
-                            )}
-                            <CustomInput
-                                placeholder="Enter your email"
-                                {...register('email')}
-                            />
-                            {errors.email && (
-                                <span className="error-message">
-                                    {errors.email.message}
-                                </span>
-                            )}
+        <section>
+            <div>
+                <ul className={css.auth_nav}>
+                    <li>
+                        <NavLink to="/auth/register">Registration</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/auth/login">Log In</NavLink>
+                    </li>
+                </ul>
 
+                <form onSubmit={formik.handleSubmit}>
+                    <div>
+                        <div>
                             <CustomInput
-                                placeholder="Enter your password"
-                                {...register('password')}
-                                type="password"
-                                onClick={passwordVisibility}
-                                showPassword={showPassword}
+                                name="username"
+                                type="text"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.username}
+                                placeholder="Enter your name"
                             />
-                            {errors.password && (
-                                <span className="error-message">
-                                    {errors.password.message}
-                                </span>
-                            )}
-                            {/* <button type="submit"> submit</button> */}
-                            <SubmitButton
-                                buttonText="Register Now"
-                                onSubmit={handleSubmit}
-                            />
+                            {formik.errors.username &&
+                                formik.touched.username && (
+                                    <div>{formik.errors.username}</div>
+                                )}
                         </div>
-                    </form>
-                </div>
-            </Container>
-        </div>
+
+                        <div>
+                            <CustomInput
+                                name="email"
+                                type="text"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.email}
+                                placeholder="Enter your email"
+                            />
+                            {formik.errors.email && formik.touched.email && (
+                                <div>{formik.errors.email}</div>
+                            )}
+                        </div>
+
+                        <div>
+                            <div className={css.inputWrapper}>
+                                <CustomInput
+                                    name="password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.password}
+                                    placeholder="Create a password"
+                                    isShowPassword={passwordVisibility}
+                                />
+                                <button
+                                    type="button"
+                                    className={css.eye}
+                                    onClick={passwordVisibility}
+                                >
+                                    <svg className={css.eyeIcon}>
+                                        <use href={`${icons}#icon-eye`}></use>
+                                    </svg>
+                                </button>
+                            </div>
+
+                            {formik.errors.password &&
+                                formik.touched.password && (
+                                    <div>{formik.errors.password}</div>
+                                )}
+                        </div>
+                    </div>
+
+                    <SubmitButton buttonText="Register Now" />
+                </form>
+            </div>
+        </section>
     );
+
+    // const navigate = useNavigate();
+    // const dispatch = useDispatch();
+    // const [showPassword, SetShowPassword] = useState(false);
+
+    // const passwordVisibility = () => {
+    //     SetShowPassword(!showPassword);
+    // };
+
+    // const schema = yup.object().shape({
+    //     name: yup.string(),
+    //     email: yup.string().email('Invalid email'),
+    //     // .required('Email is required'),
+    //     password: yup.string().min(8, 'Password must be at least 8 characters'),
+    //     // .required('Password is required'),
+    // });
+
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     // formState: { errors },
+    // } = useForm({
+    //     resolver: yupResolver(schema),
+    // });
+
+    // const onSubmit = async data => {
+    //     try {
+    //         await dispatch(signup(data));
+    //         navigate('/home');
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    //     console.log(data);
+    // };
+
+    // return (
+    //     <div className={css.register_page}>
+    //         <Container>
+    //             <div className={css.container}>
+    //                 <div>
+    //                     <ul className={css.auth_nav}>
+    //                         <li>
+    //                             <NavLink to="/auth/register">
+    //                                 Registration
+    //                             </NavLink>
+    //                         </li>
+    //                         <li>
+    //                             <NavLink to="/auth/login">Log In</NavLink>
+    //                         </li>
+    //                     </ul>
+    //                 </div>
+    //                 <form
+    //                     onSubmit={handleSubmit(onSubmit)}
+    //                     className={css.auth_form}
+    //                 >
+    //                     <div className={css.auth_div}>
+    //                         <CustomInput
+    //                             placeholder="Enter your name"
+    //                             register={register('name')}
+    //                             className={css.input}
+    //                         />
+    //                         {/* {errors.name && (
+    //                             <span className="error-message">
+    //                                 {errors.name.message}
+    //                             </span>
+    //                         )} */}
+    //                         {/* <CustomInput
+    //                             placeholder="Enter your email"
+    //                             {...register('email')}
+    //                         />
+    //                         {errors.email && (
+    //                             <span className="error-message">
+    //                                 {errors.email.message}
+    //                             </span>
+    //                         )}
+
+    //                         <CustomInput
+    //                             placeholder="Enter your password"
+    //                             {...register('password')}
+    //                             type="password"
+    //                             onClick={passwordVisibility}
+    //                             showPassword={showPassword}
+    //                         />
+    //                         {errors.password && (
+    //                             <span className="error-message">
+    //                                 {errors.password.message}
+    //                             </span>
+    //                         )} */}
+    //                         {/* <button type="submit"> submit</button> */}
+    //                         <SubmitButton
+    //                             buttonText="Register Now"
+    //                             onSubmit={handleSubmit}
+    //                         />
+    //                     </div>
+    //                 </form>
+    //             </div>
+    //         </Container>
+    //     </div>
+    // );
 };
 
 export default RegistrationForm;
--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,31 +1,18 @@
 import icons from '../../assets/icons/icons.svg';
 import scss from './CustomInput.module.scss';
 
-const CustomInput = ({ type, placeholder, name, onClick, showPassword }) => {
+const CustomInput = ({ type, placeholder, name, onChange, onBlur, value }) => {
     return (
         <>
-            {type === 'password' ? (
-                <div className={scss.inputWrapper}>
-                    <input
-                        type={showPassword ? 'text' : 'password'}
-                        name={name}
-                        placeholder={placeholder}
-                        className={scss.input}
-                    />
-                    <button className={scss.eye} onClick={onClick}>
-                        <svg className={scss.eyeIcon}>
-                            <use href={`${icons}#icon-eye`}></use>
-                        </svg>
-                    </button>
-                </div>
-            ) : (
-                <input
-                    type={type}
-                    name={name}
-                    placeholder={placeholder}
-                    className={scss.input}
-                />
-            )}
+            <input
+                type={type}
+                name={name}
+                placeholder={placeholder}
+                className={scss.input}
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+            />
         </>
     );
 };
