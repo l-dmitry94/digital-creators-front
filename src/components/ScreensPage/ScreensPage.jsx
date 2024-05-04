1cc8b62fe974ec300d6733e0ccdf432e7a522db5
Darina202 <138057834+Darina202@users.noreply.github.com>
2024-05-05 00:50:04 +0300

    screenspage-components

---
+++ src/components/ScreensPage/ScreensPage.jsx
@@ -0,0 +1,24 @@
+import StartPage from './StartPage/StartPage';
+import icons from '../../assets/icons/icons.svg';
+import scss from './ScreensPage.module.scss';
+import SubmitButton from '../SubmitButton';
+import Container from '../Container';
+import HeaderDashboard from './HeaderDashboard/HeaderDashboard';
+import MainDashboard from './MainDashboard/MainDashboard';
+
+const ScreensPage = () => {
+    return (
+        <Container>
+            <div className={scss.flex}>
+                <HeaderDashboard />
+                <MainDashboard />
+
+                <StartPage />
+
+                {/* <SubmitButton buttonText={'Add another card'} /> */}
+            </div>
+        </Container>
+    );
+};
+
+export default ScreensPage;
