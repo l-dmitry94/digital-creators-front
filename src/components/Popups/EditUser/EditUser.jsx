93c3f34e3f19140082acb62105192db2e84b6df7
Liubov <drogaltseva92@gmail.com>
2024-05-04 23:49:50 +0200

    create edit profile without style

--- src/components/Popups/EditUser/EditUser.jsx
+++ src/components/Popups/EditUser/EditUser.jsx
@@ -1,125 +1,90 @@
-import { useEffect, useState } from 'react';
-import * as yup from 'yup';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { signup } from '../../../redux/auth/auth-operations';
-import CustomInput from '../../CustomInput/CustomInput';
-import css from '../../Auth/RegisterForm/RegisterForm.module.scss';
-import icons from '../../../assets/icons/icons.svg';
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import SubmitButton from '../../SubmitButton/SubmitButton';
+import scss from './EditUser.module.scss';
+import icon from '../../../assets/icons/icons.svg';
 
-import { selectToken } from '../../../redux/auth/auth-selectors';
-import CustomForm from '../../CustomForm/CustomForm';
+import defaultImage from '../../../assets/images/user@1x-min.png';
+
+const validationSchema = Yup.object().shape({
+    username: Yup.string(),
+    email: Yup.string().email('Invalid email address'),
+    password: Yup.string().min(8, 'Password must be at least 8 characters'),
+});
 
 const EditUser = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const token = useSelector(selectToken);
-    const navigate = useNavigate();
+    const [imagePreview, setImagePreview] = useState(defaultImage);
 
-    const initialValues = {
-        username: '',
-        email: '',
-        password: '',
-    };
-
-    const validationSchema = yup.object().shape({
-        username: yup.string().required('Name is required'),
-        email: yup
-            .string()
-            .email('Invalid email')
-            .required('Email is required'),
-
-        password: yup
-            .string()
-            .min(8, 'Password must be at least 8 characters')
-            .required('Password is required'),
-    });
-
-    useEffect(() => {
-        if (token) {
-            navigate('/home');
-        }
-    }, [token, navigate]);
-
-    const passwordVisibility = () => {
-        setShowPassword(!showPassword);
-    };
     return (
-        <section>
-            <div>
-                <CustomForm
-                    initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    operation={signup}
-                    buttonText="Send"
-                >
-                    {formik => (
-                        <div>
-                            <div>
-                                <CustomInput
-                                    name="username"
-                                    type="text"
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    value={formik.values.username}
-                                    placeholder="Enter your name"
-                                />
-                                {formik.errors.username &&
-                                    formik.touched.username && (
-                                        <div>{formik.errors.username}</div>
-                                    )}
-                            </div>
+        <Formik
+            initialValues={{
+                username: '',
+                email: '',
+                password: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={values => {
+                // Обробка даних форми
+                console.log(values);
+            }}
+        >
+            {({ handleSubmit, errors, touched, setFieldValue }) => (
+                <Form onSubmit={handleSubmit} className={scss.form}>
+                    <div className={scss.boxUpload}>
+                        <img
+                            src={imagePreview}
+                            alt="User"
+                            style={{
+                                width: '68px',
+                                height: '68px',
+                                objectFit: 'cover',
+                            }}
+                        />
+                        <label className="file-input-label">
+                            <input
+                                type="file"
+                                className={scss.input}
+                                onChange={event => {
+                                    const file = event.currentTarget.files[0];
+                                    setFieldValue('image', file);
 
-                            <div>
-                                <CustomInput
-                                    name="email"
-                                    type="text"
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    value={formik.values.email}
-                                    placeholder="Enter your email"
-                                />
-                                {formik.errors.email &&
-                                    formik.touched.email && (
-                                        <div>{formik.errors.email}</div>
-                                    )}
-                            </div>
-
-                            <div>
-                                <div className={css.inputWrapper}>
-                                    <CustomInput
-                                        name="password"
-                                        type={
-                                            showPassword ? 'text' : 'password'
-                                        }
-                                        onChange={formik.handleChange}
-                                        onBlur={formik.handleBlur}
-                                        value={formik.values.password}
-                                        placeholder="Enter your password"
-                                    />
-                                    <button
-                                        type="button"
-                                        className={css.eye}
-                                        onClick={passwordVisibility}
-                                    >
-                                        <svg className={css.eyeIcon}>
-                                            <use
-                                                href={`${icons}#icon-eye`}
-                                            ></use>
-                                        </svg>
-                                    </button>
-                                </div>
-
-                                {formik.errors.password &&
-                                    formik.touched.password && (
-                                        <div>{formik.errors.password}</div>
-                                    )}
-                            </div>
-                        </div>
+                                    // Перегляд зображення для попереднього перегляду
+                                    const reader = new FileReader();
+                                    reader.onload = () => {
+                                        setImagePreview(reader.result);
+                                    };
+                                    if (file) {
+                                        reader.readAsDataURL(file);
+                                    } else {
+                                        setImagePreview(defaultImage);
+                                    }
+                                }}
+                            />
+                            <svg>
+                                <use href={`${icon}#icon-plus`}></use>
+                            </svg>
+                        </label>
+                    </div>
+                    {touched.image && errors.image && <div>{errors.image}</div>}
+                    <Field type="text" name="username" placeholder="Name" />
+                    {touched.username && errors.username && (
+                        <div>{errors.username}</div>
                     )}
-                </CustomForm>
-            </div>
-        </section>
+                    <Field type="email" name="email" placeholder="Email" />
+                    {touched.email && errors.email && <div>{errors.email}</div>}
+                    <Field
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                    />
+                    {touched.password && errors.password && (
+                        <div>{errors.password}</div>
+                    )}
+                    <SubmitButton buttonText={'Send'} />
+                </Form>
+            )}
+        </Formik>
     );
 };
 
