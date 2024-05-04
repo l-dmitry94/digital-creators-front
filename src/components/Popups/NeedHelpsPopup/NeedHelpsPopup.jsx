7aaa07708ca8a01f4e376bb15d653c4031593318
DanilKulak <1998kylak@gmail.com>
2024-05-05 00:26:47 +0300

    new-commit-unfinished

--- src/components/Popups/NeedHelpsPopup/NeedHelpsPopup.jsx
+++ src/components/Popups/NeedHelpsPopup/NeedHelpsPopup.jsx
@@ -1,38 +1,44 @@
-import React, { useState } from 'react';
 import CustomInput from '../../CustomInput/CustomInput';
-import axios from 'axios';
+import CustomForm from '../../CustomForm/CustomForm';
+import * as yup from 'yup';
 
-const NeedHelpsPopup = ({ onClose }) => {
-    const [email, setEmail] = useState('');
+import scss from './NeedHelpsPopup.module.scss';
 
-    const handleEmailChange = event => {
-        setEmail(event.target.value);
+const NeedHelpsPopup = () => {
+    const initialValues = {
+        email: '',
     };
 
-    const handleSubmit = async () => {
-        try {
-            await axios.post('/api/send-email', { email });
-            alert('Email sent successfully!');
-            onClose();
-        } catch (error) {
-            console.error('Error sending email:', error);
-            alert('Error sending email. Please try again later.');
-        }
-    };
-
+    const validationSchema = yup.object().shape({
+        email: yup
+            .string()
+            .email('Invalid email')
+            .required('Email is required'),
+    });
     return (
-        <div>
-            <CustomInput
-                type="email"
-                name="email"
-                placeholder="Email address"
-                value={email}
-                onChange={handleEmailChange}
-            />
-            <button onClick={handleSubmit}>Send</button>
-        </div>
+        <CustomForm
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            buttonText={'Send'}
+        >
+            {formik => (
+                <div>
+                    <div className={`${scss.inputContainer} ${scss.emailInput}`}>
+                        <CustomInput
+                            type="email"
+                            name="email"
+                            placeholder="Email address"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            className={scss.emailInput}
+                        />
+                        {formik.errors.email && formik.touched.email && <div>{formik.errors.email}</div>}
+                    </div>
+                </div>
+            )}
+        </CustomForm>
     );
 };
 
 export default NeedHelpsPopup;
-
