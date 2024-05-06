9524e00e1562bc87c0f511eff546a035b64e3d02
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-05-06 16:53:26 +0200

    background gallery icons created

--- src/components/Popups/CreateNewBoard/CreateNewBoard.jsx
+++ src/components/Popups/CreateNewBoard/CreateNewBoard.jsx
@@ -1,13 +1,22 @@
 import CustomInput from '../../CustomInput/CustomInput.jsx';
 import CustomRadioGroup from '../../RadioGroup/RadioButtons.jsx';
+import BackgroundGallery from '../../BackgroundGallery/BackgroundGallery.jsx';
+import scss from '../../Popups/CreateNewBoard/CreateNewBoard.module.scss';
 
 const CreateNewBoard = () => {
     return (
         <div>
-            <CustomInput placeholder={'Title'} />
-            <p>Icons</p>
-            <CustomRadioGroup />
-            <p>Background</p>
+            <div className={scss.createBoardInput}>
+                <CustomInput placeholder={'Title'} />
+            </div>
+            <p className={scss.iconsTitle}>Icons</p>
+            <div className={scss.createRadioGroup}>
+                <CustomRadioGroup />
+            </div>
+            <p className={scss.backgroundTitle}>Background</p>
+            <div className={scss.createGallery}>
+                <BackgroundGallery />
+            </div>
         </div>
     );
 };
--- src/components/RadioGroup/RadioButtons.jsx
+++ src/components/RadioGroup/RadioButtons.jsx
@@ -7,17 +7,42 @@ const CustomRadioGroup = () => {
 
     const handleOptionChange = index => {
         setSelectedOption(index);
+        // Відправка даних на сервер (яку іконку вибрав користувач)
+        sendSelectedOptionToServer(index);
+    };
+
+    const sendSelectedOptionToServer = index => {
+        const selectedIcon = radioOptions[index];
+        fetch('YOUR_SERVER_URL', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ icon: selectedIcon }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Server response:', data);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
     };
 
     const radioOptions = [
         { id: 'icon-project', title: 'Project' },
         { id: 'icon-star', title: 'Star' },
         { id: 'icon-loading', title: 'Loading' },
+        { id: 'icon-puzzle', title: 'Puzzle' },
         { id: 'icon-container', title: 'Container' },
         { id: 'icon-lightning', title: 'Lightning' },
         { id: 'icon-colors', title: 'Colors' },
         { id: 'icon-hexagon', title: 'Hexagon' },
-        { id: 'icon-puzzle', title: 'Puzzle' },
     ];
 
     return (
