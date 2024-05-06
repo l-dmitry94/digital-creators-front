13158eb226e657fc43bea97041c6c30eba71acdf
Darina202 <138057834+Darina202@users.noreply.github.com>
2024-05-07 00:00:10 +0300

    upd

--- src/components/ScreensPage/ScreensPage.jsx
+++ src/components/ScreensPage/ScreensPage.jsx
@@ -1,20 +1,17 @@
 // import StartPage from './StartPage/StartPage';
 import scss from './ScreensPage.module.scss';
 // import SubmitButton from '../SubmitButton';
-import Container from '../Container';
 import HeaderDashboard from './HeaderDashboard/HeaderDashboard';
 import MainDashboard from './MainDashboard/MainDashboard';
 
 const ScreensPage = () => {
     return (
-        <Container>
-            <div className={scss.flex}>
-                <HeaderDashboard />
-                <MainDashboard />
+        <div className={scss.flex}>
+            <HeaderDashboard />
+            <MainDashboard />
 
-                {/* <StartPage /> */}
-            </div>
-        </Container>
+            {/* <StartPage /> */}
+        </div>
     );
 };
 
