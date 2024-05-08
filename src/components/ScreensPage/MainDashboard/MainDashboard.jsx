8498709f6044bb20c73cb2fb587be0b60dd57aa5
Darina202 <138057834+Darina202@users.noreply.github.com>
2024-05-08 21:39:39 +0300

    upd

--- src/components/ScreensPage/MainDashboard/MainDashboard.jsx
+++ src/components/ScreensPage/MainDashboard/MainDashboard.jsx
@@ -2,8 +2,6 @@ import SubmitButton from '../../SubmitButton';
 import Cart from '../Cart/Cart';
 import NameColumn from '../NameColumn/NameColumn';
 import scss from './MainDashboard.module.scss';
-import SimpleBar from 'simplebar-react';
-import 'simplebar-react/dist/simplebar.min.css';
 
 const MainDashboard = () => {
     const column = [
