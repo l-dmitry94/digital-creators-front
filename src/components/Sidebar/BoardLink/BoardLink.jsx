09632beb858a10e01d53fd68a6e4f1c2b5b88ed6
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-05-09 23:55:34 +0300

    Update BoardLink.jsx

--- src/components/Sidebar/BoardLink/BoardLink.jsx
+++ src/components/Sidebar/BoardLink/BoardLink.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import scss from './BoardLink.module.scss';
 import sprite from '../../../assets/icons/icons.svg';
 import { useSelector } from 'react-redux';
@@ -16,56 +15,11 @@ const LinksListItem = ({ iconName, title, id }) => {
                 <p className={scss.linkTitle}>{title}</p>
             </div>
         </NavLink>
-        // <li
-        //     className={`${scss.linkItem} ${isChecked ? scss.checked : ''}`}
-        //     onClick={onSelect}
-        // >
-        //     <div className={scss.iconTitleFlex}>
-        //         <svg className={scss.svgIcon}>
-        //             <use xlinkHref={`${sprite}#${iconName}`} />
-        //         </svg>
-        //         <p className={scss.linkTitle}>{title}</p>
-        //     </div>
-        //     {isChecked && (
-        //         <div className={scss.iconIndicatorFlex}>
-        //             <div className={scss.svgFlex}>
-        //                 <svg className={scss.svgIconControl}>
-        //                     <use xlinkHref={`${sprite}#icon-pencil`} />
-        //                 </svg>
-        //                 <svg className={scss.svgIconControl}>
-        //                     <use xlinkHref={`${sprite}#icon-trash`} />
-        //                 </svg>
-        //             </div>
-        //             {isChecked && <div className={scss.indicator}></div>}
-        //         </div>
-        //     )}
-        // </li>
     );
 };
 
 const BoardLink = () => {
-    const [selectedItem, setSelectedItem] = useState(null);
     const boards = useSelector(selectBoards);
-    console.log(boards);
-
-    // useEffect(() => {
-    //     const savedSelectedItem = localStorage.getItem('selectedItem');
-    //     if (savedSelectedItem) {
-    //         setSelectedItem(savedSelectedItem);
-    //     } else {
-    //         setSelectedItem('Project office');
-    //     }
-    // }, []);
-
-    const handleItemClick = item => {
-        if (selectedItem === item) {
-            setSelectedItem(null);
-            localStorage.removeItem('selectedItem');
-        } else {
-            setSelectedItem(item);
-            localStorage.setItem('selectedItem', item);
-        }
-    };
 
     return (
         <ul className={scss.linkFlex}>
