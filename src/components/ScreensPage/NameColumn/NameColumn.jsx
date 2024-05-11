0f3ae34a6a433de5a16cd1db1eaaaa64d2873039
sivlvy <vladislavsidenko@ukr.net>
2024-05-11 20:07:12 +0300

    fixed

--- src/components/ScreensPage/NameColumn/NameColumn.jsx
+++ src/components/ScreensPage/NameColumn/NameColumn.jsx
@@ -2,21 +2,21 @@ import scss from './NameColumn.module.scss';
 import TodoBtn from '../TodoBtn/TodoBtn';
 import EditColumn from '../../Popups/Column/EditColumn/EditColumn';
 import CustomModal from '../../CustomModal/CustomModal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
     fetchColumns,
     removeColumn,
 } from '../../../redux/tasks/tasks-operations/tasks-columns-operations';
+import { selectColumnItems } from '../../../redux/tasks/tasks-selectors';
 const NameColumn = ({ nameColumn, boardId, columnId }) => {
     const [columnModalIsOpen, setColumnModalIsOpen] = useState(false);
-
+    const columns = useSelector(selectColumnItems);
     const dispatch = useDispatch();
 
     const handleDelete = () => {
         dispatch(removeColumn({ boardId, columnId }));
-        dispatch(fetchColumns(boardId));
     };
     const columnModalOpen = () => {
         setColumnModalIsOpen(true);
@@ -25,6 +25,11 @@ const NameColumn = ({ nameColumn, boardId, columnId }) => {
     const columnModalClose = () => {
         setColumnModalIsOpen(false);
     };
+
+    useEffect(() => {
+        dispatch(fetchColumns(boardId));
+    }, [columns, boardId, dispatch]);
+
     return (
         <div className={scss.nameColumn}>
             <p className={scss.name}>{nameColumn}</p>
